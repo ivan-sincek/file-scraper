59bcce136ca1042b07b45aaa7bde0755b20f597d
Ivan Šincek <35937483+ivan-sincek@users.noreply.github.com>
2025-01-10 17:44:29 +0100

    Exclude List Bug Fix and Version Bump

--- src/file_scraper/utils/config.py
+++ src/file_scraper/utils/config.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-APP_VERSION = "v4.4"
+APP_VERSION = "v4.5"
 
 EXCLUDES = ["car", "css", "gif", "jpeg", "jpg", "mp3", "mp4", "nib", "ogg", "otf", "eot", "png", "storyboard", "strings", "svg", "ttf", "webp", "woff", "woff2", "xib", "vtt"]
 """
@@ -13,7 +13,7 @@ def banner():
 	"""
 	print("#########################################################################")
 	print("#                                                                       #")
-	print("#                           File Scraper v4.4                           #")
+	print("#                           File Scraper v4.5                           #")
 	print("#                                   by Ivan Sincek                      #")
 	print("#                                                                       #")
 	print("# Scrape files for sensitive information.                               #")
